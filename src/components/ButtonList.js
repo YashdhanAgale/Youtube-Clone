950c8af42c202960f471e41a200fc1921410ac85
Yashodhan Agale <yash007agale@gmail.com>
2024-07-11 17:51:44 +0530

    second-commit

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,7 +1,29 @@
 import React from "react";
+import Button from "./Button";
+
+const list = [
+  "all",
+  "gaming",
+  "songs",
+  "live",
+  "cricket",
+  "SOccer",
+  "News",
+  "Lionel messi",
+  "barcelona",
+  "Football",
+  "COding",
+  "Web Development",
+];
 
 const ButtonList = () => {
-  return <div>ButtonList</div>;
+  return (
+    <div className="flex">
+      {list.map((btn) => {
+        return <Button name={btn} key={btn} />;
+      })}
+    </div>
+  );
 };
 
 export default ButtonList;
